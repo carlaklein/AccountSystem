4b78cd6aa7e7802e988e82fb08a1308b5e0c0551
Carla <klein_carla@outlook.com>
2021-10-09 19:30:44 +0200

    service dev

--- AccountSystemDomain/src/main/java/za/ac/nwu/ac/domain/dto/AccountTypeDto.java
+++ AccountSystemDomain/src/main/java/za/ac/nwu/ac/domain/dto/AccountTypeDto.java
@@ -3,6 +3,4 @@ package za.ac.nwu.ac.domain.dto;
 import java.time.LocalDate;
 
 public class AccountTypeDto {
-    public AccountTypeDto(String miles, String miles_account_type, LocalDate parse) {
-    }
 }
---
+++ AccountSystemDomain/src/main/java/za/ac/nwu/ac/domain/service/ViewMiles.java
@@ -0,0 +1,18 @@
+package za.ac.nwu.ac.domain.service;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ViewMiles {
+    public static void main(String[] args) throws IOException, SQLException {
+        String jdbcURL = "jdbc:mysql://localhost:3306/mysql";
+        String username = "root";
+        String password = "@N00dl3-klein";
+
+        Connection cnn = DriverManager.getConnection(jdbcURL, username, password);
+
+        String sql = "SELECT * FROM miles_account(acc_id, mem_id, miles_balance)";
+    }
+}
